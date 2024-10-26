6d206cb2e27314418641263e15e288b90b109830
benoy_Judy <benoyjudy@gmail.com>
2024-10-26 12:58:37 +0530

    handled state of the main form

--- src/components/mainForm/Mainform.jsx
+++ src/components/mainForm/Mainform.jsx
@@ -2,20 +2,85 @@ import React, { useEffect, useState } from "react"
 import "./mainformStyles.css"
 
 const Mainform = () => {
+	const [name, SetName] = useState("")
+	const [email, SetEmail] = useState("")
+	const [mobile, SetMobile] = useState("")
+	const [gender, setGender] = useState("")
+	const [campus, setCampus] = useState("")
+	const [checked, setChecked] = useState(false)
+
+	useEffect(() => {
+		console.log(name)
+	}, [name])
+	useEffect(() => {
+		console.log(email)
+	}, [email])
+	useEffect(() => {
+		console.log(mobile)
+	}, [mobile])
+	useEffect(() => {
+		console.log(gender)
+	}, [gender])
+	useEffect(() => {
+		console.log(campus)
+	}, [campus])
+
+	const handleGenderChange = (e) => {
+		setGender(e.target.value)
+	}
+
+	const handleCampusSelect = (e) => {
+		setCampus(e.target.value)
+		console.log(campus)
+	}
+
+	const handleChecked = (prev) => {
+		setChecked((prev) => !prev)
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		console.log({ name, email, mobile, gender, campus, checked })
+	}
+
 	return (
 		<div className="outer-box">
-			<form typeof="submit">
+			<form onSubmit={handleSubmit}>
 				<div className="heading-div">
 					<h1 className="heading">Registeration Form</h1>
 				</div>
 				<div className="input-data">
-					<input type="name" placeholder="FullName" required></input>
+					<input
+						type="name"
+						placeholder="FullName"
+						value={name}
+						onChange={(e) => {
+							SetName(e.target.value)
+						}}
+						required
+					></input>
 				</div>
 				<div className="input-data">
-					<input type="email" placeholder="Email" required></input>
+					<input
+						type="email"
+						placeholder="Email"
+						required
+						value={email}
+						onChange={(e) => {
+							SetEmail(e.target.value)
+						}}
+					></input>
 				</div>
 				<div className="input-data">
-					<input type="tel" placeholder="+91" required></input>
+					<input
+						type="tel"
+						placeholder="+91"
+						required
+						value={mobile}
+						onChange={(e) => {
+							SetMobile(e.target.value)
+						}}
+					></input>
 				</div>
 
 				<div className="gender-div">
@@ -23,25 +88,50 @@ const Mainform = () => {
 						<b>Gender:</b>
 					</label>
 					<label>
-						<input type="radio" name="gender" value="male" />
+						<input
+							type="radio"
+							name="gender"
+							value="male"
+							checked={gender === "male"}
+							onChange={handleGenderChange}
+						/>
 						Male
 					</label>
 					<label>
-						<input type="radio" name="gender" value="female" />
+						<input
+							type="radio"
+							name="gender"
+							value="female"
+							checked={gender === "female"}
+							onChange={handleGenderChange}
+						/>
 						Female
 					</label>
 					<label>
-						<input type="radio" name="gender" value="other" />
+						<input
+							type="radio"
+							name="gender"
+							value="other"
+							checked={gender === "other"}
+							onChange={handleGenderChange}
+						/>
 						Other
 					</label>
 				</div>
 				<div className="campus-data">
 					<label className="campus-label">Select Campus</label>
-					<select name="city" className="campus-select">
-						<option value="">Ernakulam</option>
-						<option value="">Chennai</option>
-						<option value="">Banglore</option>
-						<option value="">Delhi</option>
+					<select
+						name="city"
+						className="campus-select"
+						value={campus}
+						onChange={handleCampusSelect}
+						required
+					>
+						<option value=""></option>
+						<option value="Ernakulam">Ernakulam</option>
+						<option value="Chennai">Chennai</option>
+						<option value="Bengaluru">Bengaluru</option>
+						<option value="Delhi">Delhi</option>
 					</select>
 				</div>
 
@@ -50,13 +140,17 @@ const Mainform = () => {
 						type="checkbox"
 						name="updates"
 						className="checkbox-input"
+						value={checked}
+						onChange={handleChecked}
 						required
 					/>
 					I agree to the terms and conditions
 				</label>
 
 				<div className="button-div">
-					<button className="submit">submit</button>
+					<button className="submit" disabled={!checked}>
+						submit
+					</button>
 				</div>
 				<div className="ellipsis-indicator">...</div>
 			</form>
